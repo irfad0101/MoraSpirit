35329f82d1028bfd8d20df0bdc60dca688e1214a
sachithra sahan <sachithrasahansamarasinghe@gmail.com>
2015-12-13 15:31:33 +0530

    design interface for coach

--- src/moraspirit/MoraSpirit.java
+++ src/moraspirit/MoraSpirit.java
@@ -5,6 +5,8 @@
  */
 package moraspirit;
 
+import coach.CoachGUI;
+
 /**
  *
  * @author Irfad Hussain
@@ -16,6 +18,9 @@ public class MoraSpirit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        CoachGUI cgui=new CoachGUI();
+        cgui.setLocationRelativeTo(null);
+        cgui.setVisible(true);
     }
     
 }
