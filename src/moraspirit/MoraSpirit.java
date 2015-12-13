4b7298d4de77f95484b031b906306921f441d5f7
Irfad Hussain <irfad0101@gamil.com>
2015-12-13 16:45:14 +0530

    Add login window

--- src/moraspirit/MoraSpirit.java
+++ src/moraspirit/MoraSpirit.java
@@ -15,7 +15,9 @@ public class MoraSpirit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LoginFrame lf = new LoginFrame();
+        lf.setLocationRelativeTo(null);
+        lf.setVisible(true);
     }
     
 }
